cf845c41f0b862a95b9155c4d00773108e957825
Yevhenii Hrabliuk <github@nii.li>
2021-06-13 00:34:55 +0300

    Precompile some headers to speed up build a bit.

---
+++ src/util.cpp
@@ -0,0 +1,39 @@
+#include <util.hpp>
+
+namespace util
+{
+
+std::vector<std::string_view> split_lines(std::string_view view)
+{
+    std::vector<std::string_view> lines;
+    auto newline = view.find_first_of('\n');
+    for (size_t offset = 0; newline != view.npos; newline = view.find_first_of('\n', offset))
+    {
+        lines.emplace_back(view.substr(offset, newline - offset));
+        offset = newline + 1;
+    }
+    return lines;
+}
+
+std::optional<std::tuple<std::string_view, std::string_view> > split_key_and_value(std::string_view line)
+{
+    const auto sep = line.find_first_of(':');
+    if (sep == std::string_view::npos)
+    {
+        return {};
+    }
+    auto key = line.substr(0, sep);
+    auto vstart = line.find_first_not_of(' ', sep + 1);
+    if (vstart == std::string_view::npos)
+    {
+        return {};
+    }
+    auto value = line.substr(vstart, line.find_first_of('\r') - vstart);
+    if (value.empty())
+    {
+        return {};
+    }
+    return std::make_tuple(key, value);
+}
+
+}
--- src/util.hpp
+++ src/util.hpp
@@ -1,48 +1,22 @@
+#ifndef YEECTL_UTIL_HPP
+#define YEECTL_UTIL_HPP
+
 #include <string_view>
 #include <vector>
 #include <utility>
 #include <optional>
 #include <variant>
+#include <string>
 
 namespace util
 {
 
-static auto split_lines(std::string_view view)
-{
-    std::vector<std::string_view> lines;
-    auto newline = view.find_first_of('\n');
-    for (size_t offset = 0; newline != view.npos; newline = view.find_first_of('\n', offset))
-    {
-        lines.emplace_back(view.substr(offset, newline - offset));
-        offset = newline + 1;
-    }
-    return lines;
-}
-
+std::vector<std::string_view> split_lines(std::string_view view);
 // split key:value and clean any whitespace
-static auto split_key_and_value(std::string_view line) -> std::optional<std::tuple<std::string_view, std::string_view>>
-{
-    const auto sep = line.find_first_of(':');
-    if (sep == std::string_view::npos)
-    {
-        return {};
-    }
-    auto key = line.substr(0, sep);
-    auto vstart = line.find_first_not_of(' ', sep + 1);
-    if (vstart == std::string_view::npos)
-    {
-        return {};
-    }
-    auto value = line.substr(vstart, line.find_first_of('\r') - vstart);
-    if (value.empty())
-    {
-        return {};
-    }
-    return std::make_tuple(key, value);
-}
+std::optional<std::tuple<std::string_view, std::string_view>> split_key_and_value(std::string_view line);
 
 template<typename M>
-static void update_map(M & target, const M & source)
+void update_map(M & target, const M & source)
 {
     for (const auto & e : source)
     {
@@ -50,18 +24,28 @@ static void update_map(M & target, const M & source)
     }
 }
 
-static std::string variant_to_string(const auto & v)
+template<class> inline constexpr bool always_false_v = false;
+
+std::string variant_to_string(const auto & v)
 {
     return std::visit([](auto&& arg)
     {
         using T = std::decay_t<decltype(arg)>;
         if constexpr (std::is_same_v<T, std::string>)
+        {
             return arg;
+        }
         else if constexpr (std::is_arithmetic_v<T>)
+        {
             return std::to_string(arg);
+        }
         else
+        {
             static_assert(always_false_v<T>, "non-exhaustive visitor!");
+        }
     }, v);
 }
 
 }
+
+#endif // YEECTL_UTIL_HPP
